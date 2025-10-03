d0dfaf9be0322899879c98f01a6631a9d1db7431
muginito <miguel.vramos04@gmail.com>
2025-10-02 23:42:50 -0300

    corrige (atividade-03/parte-4)

--- atividade-06/parte-4/index.js
+++ atividade-06/parte-4/index.js
@@ -1,6 +1,27 @@
 // 4. setTimeout e setInterval
 //
-let i = 1;
-const contador = setInterval(() => console.log(i++), 1000);
-setTimeout(() => clearInterval(contador), 5000);
-setTimeout(() => console.log("Tempo esgotado!"), 3000);
+// let i = 1;
+// const contador = setInterval(() => console.log(i++), 990);
+// setTimeout(() => {
+//     clearInterval(contador);
+//     setTimeout(() => console.log("Tempo esgotado!"), 3000);
+// }, 5000);
+
+// Função que recebe tempo em segundos
+function contador(tempo) {
+    let i = 0;
+    const interval = setInterval(() => console.log(++i), 990);
+    setTimeout(() => {
+        clearInterval(interval);
+        setTimeout(() => console.log("Tempo esgotado!"), 3000);
+    }, (tempo * 1000));
+}
+
+contador(5);
+
+
+// OBS: Colocando o delay do setInterval em 1000 (ms), notei que o contador só mostra
+// o número anterior ao valor do delay em segundos. Diminuindo o delay para 999 o resultado passou a
+// variar entre o número correto e o seu anterior. Então, coloquei o delay em 990 para garantir.
+// Outra solução seria atrasar o timeout.
+// TESTADO NO FIREFOX
